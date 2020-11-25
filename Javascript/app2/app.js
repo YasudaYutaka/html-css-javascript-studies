6bc923e92d32a9e6b9b79a81c44ae4c9caf54b3a
YasudaYutaka <62554340+YasudaYutaka@users.noreply.github.com>
2020-11-25 10:28:47 -0300

    funcao tamanho randomico

--- Javascript/app2/app.js
+++ Javascript/app2/app.js
@@ -28,11 +28,28 @@ function randomPosition() {
 	// criar elemento HTML
 	var mosquito = document.createElement('img');
 	mosquito.src = 'imagens/mosca.png';
-	mosquito.className = 'mosquito1';
+	mosquito.className = randomSize();
 	mosquito.style.left = posicaoX + 'px';
 	mosquito.style.top = posicaoY + 'px';
 	mosquito.style.position = 'absolute';
 
 	document.body.appendChild(mosquito);
+
+}
+
+function randomSize() {
+
+	var classeMosquito = Math.floor(Math.random() * 3);
+
+	switch(classeMosquito) {
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+	}
 
 }
