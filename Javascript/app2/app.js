a3bb56ddcb2663773784a44e4ae14014b8423255
YasudaYutaka <62554340+YasudaYutaka@users.noreply.github.com>
2020-11-25 10:22:48 -0300

    funcao posicao randomica

--- Javascript/app2/app.js
+++ Javascript/app2/app.js
@@ -2,9 +2,37 @@ var altura = 0;
 var largura = 0;
 
 function ajustaTamanhoPalcoJogo() {
+
 	altura = window.innerHeight;
 	largura = window.innerWidth;
-	console.log(altura, largura);
+	console.log(largura, altura);
+
 }
 
 ajustaTamanhoPalcoJogo();
+
+function randomPosition() {
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90;
+	var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+	if (posicaoX < 0) {
+		posicaoX = 0;
+	}
+	if (posicaoY < 0) {
+		posicaoY = 0;
+	}
+
+	console.log(posicaoX, posicaoY);
+
+	// criar elemento HTML
+	var mosquito = document.createElement('img');
+	mosquito.src = 'imagens/mosca.png';
+	mosquito.className = 'mosquito1';
+	mosquito.style.left = posicaoX + 'px';
+	mosquito.style.top = posicaoY + 'px';
+	mosquito.style.position = 'absolute';
+
+	document.body.appendChild(mosquito);
+
+}
