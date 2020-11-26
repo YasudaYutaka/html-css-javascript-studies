121b5aa8e5d8a22801b838f004777ce73119a7fa
YasudaYutaka <62554340+YasudaYutaka@users.noreply.github.com>
2020-11-26 09:49:02 -0300

    setInterval - a cada 1 segundo muda posicao

--- Javascript/app2/app.js
+++ Javascript/app2/app.js
@@ -13,6 +13,11 @@ ajustaTamanhoPalcoJogo();
 
 function randomPosition() {
 
+	// remover o mosquito anterior (se existir)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove();	
+	}
+	
 	var posicaoX = Math.floor(Math.random() * largura) - 90;
 	var posicaoY = Math.floor(Math.random() * altura) - 90;
 
@@ -32,6 +37,7 @@ function randomPosition() {
 	mosquito.style.left = posicaoX + 'px';
 	mosquito.style.top = posicaoY + 'px';
 	mosquito.style.position = 'absolute';
+	mosquito.id = 'mosquito';
 
 	document.body.appendChild(mosquito);
 
