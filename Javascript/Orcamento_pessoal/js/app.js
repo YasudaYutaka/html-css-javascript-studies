423b44ef22f13bc58f1cb4709cf1036ee67357d2
YasudaYutaka <62554340+YasudaYutaka@users.noreply.github.com>
2020-12-16 10:28:52 -0300

    Listando despesas

--- Javascript/Orcamento_pessoal/js/app.js
+++ Javascript/Orcamento_pessoal/js/app.js
@@ -38,6 +38,21 @@ class Bd {
 		localStorage.setItem('id', id);
 	}
 
+	recuperarTodosRegistros() {
+		let despesas = Array();
+		let id = localStorage.getItem('id');
+		for(let i = 1; i <= id; i++) { // recuperar todas despesas do localStorage
+			//recuperar despesa
+			let despesa = JSON.parse(localStorage.getItem(i));
+			//verificar se tem indices removidos/pulados
+			if(despesa === null) {
+				continue;
+			}
+			despesas.push(despesa);
+		}
+		return despesas;
+	}
+
 }
 
 let bd = new Bd();
@@ -54,7 +69,7 @@ function cadastrarDespesa() {
 	let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value);
 
 	if(despesa.validarDados()) {
-		//bd.gravar(despesa);	
+		bd.gravar(despesa);	
 		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso';
 		document.getElementById('modal_titulo_div').className = 'modal-header text-success';
 		document.getElementById('modal_descricao').innerHTML = 'Despesa foi cadastrada com sucesso!';
@@ -69,5 +84,49 @@ function cadastrarDespesa() {
 		document.getElementById('modal_botao').className = 'btn btn-danger';
 		$('#modalRegistraDespesa').modal('show');
 	}
+
+}
+
+function carregaListaDespesa() {
+
+	let despesas = Array();
+	despesas = bd.recuperarTodosRegistros();
+
+	// selecionando elemento tbody da tabela
+	let listaDespesas = document.getElementById('listaDespesas');
+
+	// percorrer array despesas, listando os elementos de forma dinâmica
+	despesas.forEach(function(d) {
+
+		// criando o tr (linha)
+		let linha = listaDespesas.insertRow();
+		// criar o td (coluna)
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+
+		
+		// ajustar tipo
+		switch(d.tipo) {
+			case '1':
+				d.tipo = 'Alimentação';
+				break;
+			case '2':
+				d.tipo = 'Educação';
+				break;	
+			case '3':
+				d.tipo = 'Lazer';
+				break;
+			case '4':
+				d.tipo = 'Saúde';
+				break;
+			case '5':
+				d.tipo = 'Transporte';
+				break;		
+		}
+		linha.insertCell(1).innerHTML = d.tipo;
+
+		linha.insertCell(2).innerHTML = d.descricao;
+		linha.insertCell(3).innerHTML = d.valor;
+
+	})
 
 }
